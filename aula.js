25b69e020ca64593b06e4196fddeec496b1d944f
SkSamuel157 <samuelscs12senai@gmail.com>
2023-08-07 14:31:03 -0300

    Feito
    🍀🍀🍀

---
+++ aula.js
@@ -0,0 +1,18 @@
+// Math.random gera um número aleatorio
+console.log(Math.random());
+
+// Gerando um Nuúmero aleatória entre 0 e 20
+console.log(Math.random() * 20);
+console.log(Math.random(Math.random() * 5));
+
+// Math.round arredonda para  o inteirto mais proxímo
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+// Math.ceil arredonda para cima
+console.log(Math.ceil(3.1));
+console.log(Math.ceil(3.9));
+
+// Math.floor arredonda para baixo
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9));
